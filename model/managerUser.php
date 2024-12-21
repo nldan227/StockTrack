c4e8b9e734dbf01c33d739aa97ecbdd1724aa8c5
Linh Dan Nguyen <nguyenlinhdan227@gmail.com>
2024-12-21 17:55:18 +0700

    Update: Fixed logic in backend/<file_name>

--- model/managerUser.php
+++ model/managerUser.php
@@ -357,7 +357,7 @@ if(isset($_SESSION['id'])){
                     <thead>
                         <tr>
                             <th>ID</th>
-                            <th>Usernamecolen </th>
+                            <th>Username</th>
                             <th>Họ tên</th>
                             <th>Số điện thoại</th>
                             <th>Vai Trò</th>
